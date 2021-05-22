dd1437c7c4a50008c86a8e41cebfb06217fb7d03
LeninRam0s <djalexander.rmix@gmail.com>
2021-05-21 22:59:20 -0600

    Agregar archivos de proyecto.

---
+++ culebrita/Persistencia/Archivo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JuegoSnake.clases
+{
+    class Archivo
+    {
+        String path;
+
+        public Archivo()
+        {
+            path = @"G:\1.  UNIVERSIDAD 2021\1 PROGRAMACION\PROYECTOSC-SHARP\culebrita\culebrita\culebrita\Persistencia\data.txt";
+        }
+        public int punteo()
+        {
+            TextReader leer = new StreamReader(path);
+            int dato = Convert.ToInt32(leer.ReadLine());
+            leer.Close();
+            return dato;
+            
+        }
+        public int mejorPunteo(int punteoActual)
+        {
+            if(punteoActual > punteo())
+            {
+                TextWriter escribir = new StreamWriter(path);
+                escribir.WriteLine(punteoActual);
+                escribir.Close();
+                return punteo();
+            }
+            else
+            {
+                return punteo();
+            }
+        }
+    }
+}
